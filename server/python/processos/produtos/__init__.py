b4051439e9d2006a5c2907d2f7690396153b57a2
Enio-Telles <eniotelles@gmail.com>
2026-03-21 07:45:30 -0400

    refactor: cria estrutura modular de processos

---
+++ server/python/processos/produtos/__init__.py
@@ -0,0 +1,9 @@
+"""
+Processo de produtos.
+
+Subprocessos:
+- separacao: normalização e organização de produtos base;
+- agregacao: consolidação/agrupamento de descrições e chaves;
+- fatores_conversao: cálculo e revisão de fatores entre unidades;
+- orquestrador: coordenação da execução.
+"""
